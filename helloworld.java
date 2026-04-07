3bfe39dc5e7ad3878538fd479511baf7cf0f3a07
Hellowo45698 <saritahesarur2@gmail.com>
2026-04-07 04:10:10 -0700

    first

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    // The main method is the entry point where execution starts
+    public static void main(String[] args) {
+        // This command prints text to the console
+        System.out.println("Hello, World!");
+    }
+}
